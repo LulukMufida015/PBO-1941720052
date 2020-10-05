91d7d346d5f3682f2f7e1eb0cdb1a3750e06205d
Luluk Mufida <lulukmufida0015@gmail.com>
2020-10-05 14:55:16 +0700

    Praktikum Pertemuan 6

---
+++ pertemuan6/jobsheet6/src/percobaan2/ClassA.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package percobaan2;
+
+/**
+ *
+ * @author Windows 10
+ */
+public class ClassA {
+    public int x;
+    public int y;
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+    public void getNilai(){
+        System.out.println("Nilai x: "+ x);
+        System.out.println("Nilai y: "+ y);
+    }
+}
